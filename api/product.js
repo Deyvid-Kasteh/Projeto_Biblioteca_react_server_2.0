5031146ce729df9f97f6ec07b9bbfe81ee213164
Deyvid Kasteh <kasteh21@gmail.com>
2023-01-05 15:03:48 -0300

    first commit

---
+++ api/product.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        res.json({
+            status: 200,
+            message: "Get data has successfully returned"
+        });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send('Server error');
+    }
+})
+module.exports = router;
